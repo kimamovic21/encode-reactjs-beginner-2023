f1bf5dc2b14d8cf08ea87b7117bb8b8cd5870295
Kerim Imamovic <79793398+kimamovic21@users.noreply.github.com>
2023-10-19 23:20:53 +0200

    Day 02
    -kreiranje foldera 02_todo_app

---
+++ 02_todo_app/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import './App.css';
+
+const App = () => {
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleTaskChange = (event) => {
+    // console.log(event);
+    // console.log(event.target);
+    const { value } = event.target;
+    // console.log(value);
+
+    setTask(value);
+  };
+
+  const handleSave = () => {
+    // shallow copy
+      const newTodos = [...todos, task];
+      console.log(newTodos);
+
+      setTodos(newTodos);
+      setTask('');
+      // setTodos((prevValue) => [...prevValue, tasl]);
+  };
+
+  // console.log('todos:', todos);
+
+  const handleDelete = (index) => {
+    const todosCopy = [...todos];
+
+    // obrisali element na poziciji index i deleteCount je 1
+    todosCopy.splice(index, 1);
+    console.log(todosCopy);
+
+    setTodos(todosCopy);
+  };
+
+  return (
+    <div className="App">
+        <div>
+          <input 
+              type="text" 
+              value={task}
+              onChange={(event) => handleTaskChange(event)} 
+          />
+          <button 
+              disabled={task.length < 5}
+              className="create-button"
+              onClick={() => {
+                handleSave();
+              }}
+          >
+            Create
+          </button>
+        </div>
+        <div>
+          {todos.map((todo, index) => {
+            // console.log(todo, index);
+            return (
+              <div key={index} className="todo-wrapper">
+                <p>{todo}</p>
+                <button className="delete-button" onClick={() => handleDelete()}>
+                  Delete
+                </button>
+              </div>
+            )
+          })}
+        </div>
+    </div>
+  );
+};
+
+export default App;
